60787a75f329d95b7508cc52a7be2f3425de975f
ntminhchau <chaunminh@gmail.com>
2026-04-02 02:28:41 +0700

    Create vn100_list.py

---
+++ vn100_list.py
@@ -0,0 +1,15 @@
+VN100 = [
+    "VNM","VPB","VCB","HPG","FPT","STB","VIX","MBB","SHB","VHM","VIC","MSN",
+    "SSI","HDB","PLX","HCM","MWG","BID","CTG","DGW","ACB","BSR","GAS","TCB",
+    "GEX","VRE","IDC","TPB","POW","VND","GVR","VGI","VJC","VCI","VTP","KBC",
+    "VSC","CEO","DXG","ACV","PVS","MSR","SAB","VCK","FOX","DGC","SHS","CII",
+    "EIB","GMD","CTR","MBS","KDH","BAF","DBC","PDR","VEA","HAG",
+    "PVD","VPI","BCM","OIL","PC1","HAH","PET","DIG","VHC","VCG","VIB","BVH",
+    "TCH","DCM","PNJ","FRT","GEE","DPM","NVL","PLC","NTP","NKG","NT2","BMP",
+    "PVT","TCX","MSB","VPX","HDC","HHV","DPG","HVN","LCG","LPB","PHR","VPL",
+    "ELC","CTS","HSG","DRI","HDG","NLG","TNG","REE","SIP","CTD","VGC","DPR",
+    "DCL","VTZ","MCH","SZC","ANV","FTS","IJC","TCM","CSV","DDV","VGS","SCS",
+    "SSB","OCB","PVC","KSV","EVF","PAN","BSI","BVB","VLC","PVI","KOS","CTI",
+    "NAB","VC3","VDS","NAF","ABB","VGT","KSB","CMG","DXP","MIG","AGR","HAX",
+    "HHS","SBT","HUT","BWE","MSH","BFC","NTL"
+]
